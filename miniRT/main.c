c0ad33ff4ab4aa262d5c652f7ec6a0b3516d1c7d
Hee young Kang <hekang@student.42seoul.kr>
2020-12-01 14:01:32 +0900

    miniRT_2

--- miniRT/main.c
+++ miniRT/main.c
@@ -1,4 +1,5 @@
 #include <mlx.h>
+#include <stdio.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -6,6 +7,8 @@ typedef struct	s_data {
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+	void		*mlx;
+	void		*win;
 }				t_data;
 
 void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -14,6 +17,12 @@ void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
+}
+
+int             key_hook(int keycode, t_data *vars)
+{
+    printf("Hello from key_hook!\n");
+	return (1);
 }
 
 int		main(void)
@@ -27,11 +36,13 @@ int		main(void)
 	img.img = mlx_new_image(mlx, 1920, 1080);// 이미지 instance 생성
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 
-	for(int i = 0; i < 100 ; i++) {
-		my_mlx_pixel_put(&img, i, i, 0x00FF0000);// 붉은색 선을 대각선으로 그린다.
-		my_mlx_pixel_put(&img, 5, i, 0x00FF0000);// 붉은색 선을 세로으로 그린다.
-		my_mlx_pixel_put(&img, i, 5, 0x00FF0000);// 붉은색 선을 가로으로 그린다.
+	for(int i = 0; i < 1080 ; i++) {
+		my_mlx_pixel_put(&img, i, i, 0x00FF0000);// ARGB(alpha red green blue)
+		my_mlx_pixel_put(&img, i, i, 0x00FF0000);// 붉은색 선을 세로으로 그린다.
+		my_mlx_pixel_put(&img, i, i, 0x00FF0000);// 붉은색 선을 가로으로 그린다.
 	}
+
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);//이미지를 윈도우에 올린다.
+	mlx_key_hook(mlx_win, key_hook, &img);
 	mlx_loop(mlx);
 }
